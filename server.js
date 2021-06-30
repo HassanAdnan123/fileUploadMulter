b0265753dbb8f1db6bb4749ea99cc7e75cba43cd
Hassan Adnan <43107637+HassanAdnan123@users.noreply.github.com>
2021-06-30 14:55:13 +0500

    First Commit
    Commit 1

---
+++ server.js
@@ -0,0 +1,49 @@
+var express = require('express')
+var cors = require('cors')
+var multer  = require('multer')
+
+
+var storage = multer.diskStorage(
+    {
+        destination: './uploads/',
+        filename: function ( req, file, cb ) {
+            cb( null, file.originalname+ '-' + Date.now()+".jpg");
+        }
+    }
+);
+
+var upload = multer({storage: storage});
+
+var app = express()
+
+var corsConfig = {
+    origin: "http://localhost:8888"
+};
+
+app.use(cors(corsConfig));
+
+const port = process.env.PORT || 8888;
+app.listen(port, () => {
+  console.log('Connected to port ' + port)
+})
+
+app.post('/profile', upload.single('avatar'), function (req, res, next) {
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+})
+
+app.post('/photos/upload', upload.array('photos', 12), function (req, res, next) {
+  // req.files is array of `photos` files
+  // req.body will contain the text fields, if there were any
+})
+
+var cpUpload = upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'gallery', maxCount: 8 }])
+app.post('/cool-profile', cpUpload, function (req, res, next) {
+  // req.files is an object (String -> Array) where fieldname is the key, and the value is array of files
+  //
+  // e.g.
+  //  req.files['avatar'][0] -> File
+  //  req.files['gallery'] -> Array
+  //
+  // req.body will contain the text fields, if there were any
+})
